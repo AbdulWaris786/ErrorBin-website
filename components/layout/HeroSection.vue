48c1a86ab0e00affa132c6a96c4202f8acc33f81
abdul waris <warismuthuparamba@gmail.com>
2025-02-11 19:00:03 +0530

    completed header

---
+++ components/layout/HeroSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="h-[100vh] bg-black w-[100vw]">
+        <div class="h-full w-full flex ">
+            <div class="h-[100%] w-[50%] flex">
+                <div class="h-full w-[25%] bg-green-300"></div>
+                <div class="h-full w-[75%] pt-[20%] bg-green-500">
+                    <div class="bg-red-300">
+                        <h1 class="text-[50px] text-white font-bold">BUILD YOUR FUTURE</h1>
+                        <h1 class="text-[50px] text-white font-bold">BUSINESS WITH US</h1>
+                    </div>
+                    <br><br>
+                    <div class="bg-green-800 flex items-center justify-center h-[50px] ">
+                        <p class="text-white text-[30px] font-bold">WEB DEVELOPMENT</p>
+                    </div>
+                    <br><br>
+                    <div class=""></div>
+                </div>
+            </div>
+            <div class="flex justify-center items-center">
+                <div class="h-[200px] w-[200px] bg-white blur-[150px]"></div>
+            </div>
+            <div class="h-[100%] w-[50%]">
+
+            </div>
+           
+        </div>
+
+    </div>
+</template>
